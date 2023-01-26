20bb2951936d5ba7f5025fe942db0298eb7c112b
Asger Svenning <asgersvenning@gmail.com>
2023-01-26 16:15:20 +0100

    Fix directory error in docker CI.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,5 @@
+cd deploy/
+
 docker build -f Dockerfile_base --progress=plain -t learndfv_base .
 docker build -f Dockerfile --progress=plain -t learndfv:latest .
 docker run -p 80:80 learndfv:latest
