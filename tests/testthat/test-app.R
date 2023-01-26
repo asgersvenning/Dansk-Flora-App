0ba60f1bb7fb9e165f5c6dc63309b37317160658
Asger Svenning <asgersvenning@gmail.com>
2023-01-26 17:18:14 +0100

    Try to implement a test docker run for github actions to run a testServer instead of starting the app.

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(learndfv)
+library(shiny)
+
+testServer(shinyApp(app_ui, app_server), {
+  test1 <- session$setInputs(fetchSpecies = 1)
+  if (!test1) stop("Couldn't fetch species in species server!")
+  
+  print("All good!")
 })
